4ccadf68302f4c56c2c135b4e6829a69e95b6edd
daiki-kon <61902151+daiki-kon@users.noreply.github.com>
2023-05-04 13:43:06 +0900

    iterationの章実施

---
+++ iteration/repeat.go
@@ -0,0 +1,13 @@
+package iteration
+
+func Repeat(character string, count int) string {
+
+    var repeated string
+
+    for i := 0; i < count; i++ {
+        repeated += character
+    }
+
+	return repeated
+
+}
